101d85fd158cd074a41323f119d93e174a310b0f
DESKTOP-GPJCRMT\aulas <dsad@dasd.cs>
2018-02-22 11:16:00 +0000

    Adicionar algumas variáveis do tipo string

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -10,6 +10,14 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            string s = "Unicode \u0b80";
+            string h = "Copyright \u0ce0";
+            string d = "Unicode \x0040";
+
+            Console.WriteLine(s);
+            Console.WriteLine(h);
+            Console.WriteLine(d);
+            Console.ReadKey();
         }
     }
 }
